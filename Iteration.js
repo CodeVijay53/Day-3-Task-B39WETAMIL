922f92920f83f6709cd478ef749861827f53aeb2
Vijay Shankar <91515683+CodeVijay53@users.noreply.github.com>
2022-08-26 08:34:25 +0530

    Add files via upload

---
+++ Iteration.js
@@ -0,0 +1,58 @@
+//For Iteration of data
+//-------------------------------//
+var Biofor = [{
+    name: "Vijay",
+    Age: "26",
+    dob: "09-07-1995",
+    Education: "Mechanical Engineering",
+    Year: "2017",
+    skills : "CAD"
+}];
+
+for (var i=0; i < Biofor.length; i++){
+    console.log(Biofor[i]);
+}
+
+// For in Iteration of a data
+//-------------------------------//
+var Bioforin = {
+    name: "Vijay",
+    Age: "26",
+    dob: "09-07-1995",
+    Education: "Mechanical Engineering",
+    Year: "2017",
+    skills : "CAD"
+};
+
+for (const key in Bioforin){
+    console.log(key + " : "+ Bioforin[key]);
+}
+
+// For of Iteration of data
+//-------------------------------//
+var Bioforof = {
+    name: "Vijay",
+    Age: "26",
+    dob: "09-07-1995",
+    Education: "Mechanical Engineering",
+    Year: "2017",
+    skills : "CAD"
+};
+
+for (const key of Object.keys(Bioforof)){
+    console.log(key + " : " + Bioforof[key])
+}
+
+// For Each Iteration of data
+//-------------------------------//
+var Bioforeach = {
+    name: "Vijay",
+    Age: "26",
+    dob: "09-07-1995",
+    Education: "Mechanical Engineering",
+    Year: "2017",
+    skills : "CAD"
+};
+Object.keys(Bioforeach).forEach(key=> {
+    console.log(key, " : " + Bioforeach[key]);
+});
